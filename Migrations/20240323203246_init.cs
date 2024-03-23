dee8bc4305d42ba8a191f3ed38d934e543153b0d
zaidsaadedin1 <zaidsaadedin11@gmail.com>
2024-03-23 23:38:09 +0300

    add migrations

---
+++ Migrations/20240323203246_init.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CodeMasters.Migrations
+{
+    /// <inheritdoc />
+    public partial class init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Orders",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    LastName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    University = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Major = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PhoneNumber = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CreateAt = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    StartAtDate = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    DeadLineDate = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Orders", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Orders");
+        }
+    }
+}
--- obj/Debug/net8.0/CodeMasters.AssemblyInfo.cs
+++ obj/Debug/net8.0/CodeMasters.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CodeMasters")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dd41e12eb68ebfe70f5ab435c4d368ef4e4bf609")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+18f0679c1c29f073b5b00e3a8ac854b0fbb90cfe")]
 [assembly: System.Reflection.AssemblyProductAttribute("CodeMasters")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CodeMasters")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
